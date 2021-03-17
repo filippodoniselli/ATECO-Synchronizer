aa84817a83acc9c4b7cdadb85c245a4f5b8589c8
Filippo <filidoni97@gmail.com>
2021-03-17 18:30:26 +0100

    ASF-16 Creazione cartella logs (+ correzione path)
    Creazione cartella logs nella directory dell'applicativo e correzione nella gestione della posizione dei file IRP-AIRAP in ingresso

--- AtecoSynchronizer/Program.cs
+++ AtecoSynchronizer/Program.cs
@@ -10,9 +10,12 @@ namespace AtecoSynchronizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string[] IRP = Directory.GetFiles(ConfigurationManager.AppSettings["IRP"]), AIRAP = Directory.GetFiles(ConfigurationManager.AppSettings["AIRAP"]);
+            DirectoryInfo logs = new DirectoryInfo(Directory.GetCurrentDirectory()+"\\logs");
+            if (!Directory.Exists(logs.ToString()))
+                logs.Create();
+            string IRP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["IRP"], AIRAP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["AIRAP"];
         }
     }
 }
