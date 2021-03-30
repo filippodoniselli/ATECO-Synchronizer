fe6299d874097634ff092cb1734e9e8a017c187e
filippodoniselli <filidoni97@gmail.com>
2021-03-30 14:15:18 +0200

    ASF-17 + ASF-18 Scrittura corpo e Unit Test
    Completata la scrittura dei metodi di controllo e conversione dei file di input con relativi unit test. Corretti anche errori in app.config legati al logging

--- AtecoSynchronizer/Program.cs
+++ AtecoSynchronizer/Program.cs
@@ -1,34 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
 using System.Configuration;
+using System.IO;
 using NPOI.HSSF.UserModel;
-using NPOI.XSSF.UserModel;
 using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Linq;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace AtecoSynchronizer
 {
-    class Program
+    public class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main()
         {
-            DirectoryInfo logs = new DirectoryInfo(Directory.GetCurrentDirectory()+"\\logs");
-            if (!Directory.Exists(logs.ToString()))
-                logs.Create();
+            log.Info("Program Running");
             string IRP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["IRP"], AIRAP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["AIRAP"];
-        }
-        public Boolean WellFormedXLS(string path)
-        {
-            if (path.Contains("\\.xls"))
+            string Cod_Att_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["Cod_Att"];
+            string[] IRP_files = Directory.GetFiles(IRP_path), AIRAP_files = Directory.GetFiles(AIRAP_path);
+            if (File.Exists(Cod_Att_path) == false || IRP_files == null || AIRAP_files == null)
             {
-                FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read);
+                log.Warn("Files not found in specified directory");
+                return;
+            }
+            if (IRP_files.Length != AIRAP_files.Length)
+            {
+                log.Warn("Uncorrect number of input files");
+                return;
+            }
+            int i;
+            if (WellFormed(Cod_Att_path) == false)
+                return;
+            List<Tuple<string, string>> Cod_Att = Convert(Cod_Att_path);
+            for (i = 0; i < IRP_files.Length; i++)
+            {
+                if (WellFormed(IRP_files[i]) == true && WellFormed(AIRAP_files[i]) == true)
+                {
+                    List<Tuple<string, string>> IRP = Convert(IRP_files[i]), AIRAP = Convert(AIRAP_files[i]);
+                    DirectoryInfo result = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\_Result_");
+                    result.Create();
+                }
+            }
+        }
+        public static bool WellFormed(string path)
+        {
+            FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read);
+            if (path.EndsWith(".xls"))
+            {
                 HSSFWorkbook workbook = new HSSFWorkbook(input);
                 ISheet sheet = workbook.GetSheetAt(0);
-
+                if (CheckingInstructionSet(sheet) == false)
+                {
+                    log.Warn("Error in " + path);
+                    return false;
+                }
+                else
+                    return true;
             }
+            else if (path.EndsWith(".xlsx"))
+            {
+                XSSFWorkbook workbook = new XSSFWorkbook(input);
+                ISheet sheet = workbook.GetSheetAt(0);
+                if (CheckingInstructionSet(sheet) == false)
+                {
+                    log.Warn("Error in " + path);
+                    return false;
+                }
+                return true;
+            }
+            else
+            {
+                log.Warn("Uncorrect extension: " + path);
+                return false;
+            }
+        }
+        public static bool CheckingInstructionSet(ISheet sheet)
+        {
+            int i;
+            IRow row = sheet.GetRow(0); ;
+            switch (row.PhysicalNumberOfCells)
+            {
+                case 2:
+                    for (i = 0; i <= sheet.LastRowNum; i++)
+                    {
+                        row = sheet.GetRow(i);
+                        if (row.GetCell(0).CellType != CellType.String || row.GetCell(1).CellType != CellType.String || String.IsNullOrWhiteSpace(row.GetCell(0).StringCellValue) == true || String.IsNullOrWhiteSpace(row.GetCell(1).StringCellValue) == true)
+                            return false;
+                    }
+                    break;
+                case 4:
+                case 6:
+                case 8:
+                    for (i = 0; i <= sheet.LastRowNum; i++)
+                    {
+                        row = sheet.GetRow(i);
+                        if (row.GetCell(0).StringCellValue == "XX")
+                            return true;
+                        if (row.GetCell(0).CellType != CellType.String || row.GetCell(2).CellType != CellType.String || String.IsNullOrWhiteSpace(row.GetCell(0).StringCellValue) == true  || String.IsNullOrWhiteSpace(row.GetCell(2).StringCellValue) == true)
+                            return false;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
+        public static List<Tuple<string, string>> Convert(string path)
+        {
+            FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read);
+            if (path.EndsWith(".xls"))
+            {
+                HSSFWorkbook workbook = new HSSFWorkbook(input);
+                ISheet sheet = workbook.GetSheetAt(0);
+                return ConversionInstructionSet(sheet);
+            }
+            else
+            {
+                XSSFWorkbook workbook = new XSSFWorkbook(input);
+                ISheet sheet = workbook.GetSheetAt(0);
+                return ConversionInstructionSet(sheet);
+            }
+        }
+        public static List<Tuple<string, string>> ConversionInstructionSet(ISheet sheet)
+        {
+            var result = new List<Tuple<string, string>>(sheet.LastRowNum);
+            int i;
+            IRow row = sheet.GetRow(0);
+            if (row.PhysicalNumberOfCells == 2)
+            {
+                for(i=0; i <= sheet.LastRowNum; i++)
+                {
+                    row = sheet.GetRow(i);
+                    var item = new Tuple<string, string>(row.GetCell(0).StringCellValue, row.GetCell(1).StringCellValue);
+                    result.Add(item);
+                }
+            }
+            else
+            {
+                for(i=0; i <= sheet.LastRowNum; i++)
+                {
+                    row = sheet.GetRow(i);
+                    if (row.GetCell(0).StringCellValue == "XX")
+                        break;
+                    var item = new Tuple<string, string>(row.GetCell(0).StringCellValue, row.GetCell(2).StringCellValue);
+                    result.Add(item);
+                }
+            }
+            return result;
         }
     }
 }
--- AtecoSynchronizer_Test/UnitTest1.cs
+++ AtecoSynchronizer_Test/UnitTest1.cs
@@ -1,14 +1,147 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using AtecoSynchronizer;
 
 namespace AtecoSynchronizer_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class Program_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckingIS_2_Rows_True()
         {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell10 = row1.CreateCell(0), cell11 = row1.CreateCell(1);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell10.SetCellValue("1");
+            cell11.SetCellValue("1");
+            Assert.IsTrue(Program.CheckingInstructionSet(sheet));
         }
+        [TestMethod]
+        public void CheckingIS_2_Rows_CellTypeError()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell10 = row1.CreateCell(0), cell11 = row1.CreateCell(1);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell10.SetCellValue("1");
+            Assert.IsFalse(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void CheckingIS_2_Rows_NullOrWhiteError()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell10 = row1.CreateCell(0), cell11 = row1.CreateCell(1);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell10.SetCellValue("1");
+            cell11.SetCellValue(" ");
+            Assert.IsFalse(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void CheckingIS_4_6_8_Rows_True()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell02 = row0.CreateCell(2), cell03 = row0.CreateCell(3);
+            ICell cell10 = row1.CreateCell(0), cell12 = row1.CreateCell(2);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell02.SetCellValue("-");
+            cell03.SetCellValue("-");
+            cell10.SetCellValue("1");
+            cell12.SetCellValue("1");
+            Assert.IsTrue(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void CheckingIS_4_6_8_Rows_CellTypeError()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell02 = row0.CreateCell(2), cell03 = row0.CreateCell(3);
+            ICell cell10 = row1.CreateCell(0), cell12 = row1.CreateCell(2);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell02.SetCellValue("-");
+            cell03.SetCellValue("-");
+            cell10.SetCellValue("1");
+            Assert.IsFalse(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void CheckingIS_4_6_8_Rows_NullOrWhiteError()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell02 = row0.CreateCell(2), cell03 = row0.CreateCell(3);
+            ICell cell10 = row1.CreateCell(0), cell12 = row1.CreateCell(2);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell02.SetCellValue("-");
+            cell03.SetCellValue("-");
+            cell10.SetCellValue("");
+            cell12.SetCellValue("1");
+            Assert.IsFalse(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void CheckingIS_UncorrectNum_Rows()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell02 = row0.CreateCell(2);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell02.SetCellValue("-");
+            Assert.IsFalse(Program.CheckingInstructionSet(sheet));
+        }
+        [TestMethod]
+        public void ConversionIS_2_Rows()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell10 = row1.CreateCell(0), cell11 = row1.CreateCell(1);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell10.SetCellValue("1");
+            cell11.SetCellValue("1");
+            var item1 = new Tuple<string, string>( "-", "-");
+            var item2 = new Tuple<string, string>( "1" , "1");
+            Assert.IsTrue(Program.ConversionInstructionSet(sheet).IndexOf(item1) == 0 && Program.ConversionInstructionSet(sheet).IndexOf(item2) == 1);
+        }
+        [TestMethod]
+        public void ConversionIS_4_6_8_Rows()
+        {
+            HSSFWorkbook workbook = new HSSFWorkbook();
+            ISheet sheet = workbook.CreateSheet();
+            IRow row0 = sheet.CreateRow(0), row1 = sheet.CreateRow(1);
+            ICell cell00 = row0.CreateCell(0), cell01 = row0.CreateCell(1), cell02 = row0.CreateCell(2), cell03 = row0.CreateCell(3);
+            ICell cell10 = row1.CreateCell(0), cell11 = row1.CreateCell(1), cell12 = row1.CreateCell(2), cell13 = row1.CreateCell(3);
+            cell00.SetCellValue("-");
+            cell01.SetCellValue("-");
+            cell02.SetCellValue("-");
+            cell03.SetCellValue("-");
+            cell10.SetCellValue("1");
+            cell12.SetCellValue("1");
+            var item1 = new Tuple<string, string>("-", "-");
+            var item2 = new Tuple<string, string>("1", "1");
+            Assert.IsTrue(Program.ConversionInstructionSet(sheet).IndexOf(item1) == 0 && Program.ConversionInstructionSet(sheet).IndexOf(item2) == 1);
+        }
+
     }
 }
