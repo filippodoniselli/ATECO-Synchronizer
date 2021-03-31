316c9eadea91ee7ead5acf2423f28df7e923171c
filippodoniselli <filidoni97@gmail.com>
2021-03-31 19:03:45 +0200

    Ottimizzazione del lavoro svolto fino ad ora
    Seguiti alcuni dei consigli ricevuti e adoperato delle correzioni

--- AtecoSynchronizer/Program.cs
+++ AtecoSynchronizer/Program.cs
@@ -17,80 +17,67 @@ namespace AtecoSynchronizer
         static void Main()
         {
             log.Info("Program Running");
-            string IRP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["IRP"], AIRAP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["AIRAP"];
+            string IRP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["IRP"];
+            string AIRAP_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["AIRAP"];
             string Cod_Att_path = Directory.GetCurrentDirectory() + ConfigurationManager.AppSettings["Cod_Att"];
             string[] IRP_files = Directory.GetFiles(IRP_path), AIRAP_files = Directory.GetFiles(AIRAP_path);
-            if (File.Exists(Cod_Att_path) == false || IRP_files == null || AIRAP_files == null)
+            if (!File.Exists(Cod_Att_path) || 
+                IRP_files == null ||
+                AIRAP_files == null)
             {
                 log.Warn("Files not found in specified directory");
                 return;
             }
-            if (IRP_files.Length != AIRAP_files.Length)
-            {
-                log.Warn("Uncorrect number of input files");
-                return;
-            }
-            int i;
-            if (WellFormed(Cod_Att_path) == false)
+            if (!WellFormed(Cod_Att_path))
             {
                 log.Error("Cod_Att bad formed. Impossible to generate output");
                 return;
             }
             List<Tuple<string, string>> Cod_Att = Convert(Cod_Att_path);
-            for (i = 0; i < IRP_files.Length; i++)
+            int i;
+            for(i=0; i < IRP_files.Length; i++)
             {
-                if (WellFormed(IRP_files[i]) == true && WellFormed(AIRAP_files[i]) == true)
+                if ( WellFormed(IRP_files[i]) && WellFormed(AIRAP_files[i]))
                 {
                     List<Tuple<string, string>> IRP = Convert(IRP_files[i]), AIRAP = Convert(AIRAP_files[i]);
-                    DirectoryInfo result = new DirectoryInfo(Directory.GetCurrentDirectory() + "\\_Result_");
+                    DirectoryInfo result = new DirectoryInfo(Directory.GetCurrentDirectory() + @"\_Result_");
                     result.Create();
                 }
             }
         }
         public static bool WellFormed(string path)
         {
-            bool response = true;
-            FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read);
-            if (path.EndsWith(".xls"))
+            log.Info("Opening to check if well formed: " + path);
+            if (!path.Contains(".xls"))
             {
-                HSSFWorkbook workbook = new HSSFWorkbook(input);
-                ISheet sheet = workbook.GetSheetAt(0);
-                if (CheckingInstructionSet(sheet) == false)
-                {
-                    log.Warn("Error in " + path);
-                    response = false;
-                }
+                log.Warn("Uncorrect extension of:" + path);
+                return false;
             }
-            else if (path.EndsWith(".xlsx"))
-            {
-                XSSFWorkbook workbook = new XSSFWorkbook(input);
-                ISheet sheet = workbook.GetSheetAt(0);
-                if (CheckingInstructionSet(sheet) == false)
-                {
-                    log.Warn("Error in " + path);
-                    response = false;
-                }
-            }
-            else
-            {
-                log.Warn("Uncorrect extension: " + path);
-                response = false;
-            }
+            ISheet sheet = path.EndsWith(".xls") ?
+                new HSSFWorkbook(new FileStream(path, FileMode.Open, FileAccess.Read)).GetSheetAt(0) : 
+                new XSSFWorkbook(new FileStream(path, FileMode.Open, FileAccess.Read)).GetSheetAt(0);
+            bool response = CheckingInstructionSet(sheet);
+            log.Info(response ? "Well formed" : "Bad formed");
             return response;
         }
         public static bool CheckingInstructionSet(ISheet sheet)
         {
+            log.Info("Checking file");
             int i;
             bool response = true;
-            IRow row = sheet.GetRow(0);
-            switch (row.PhysicalNumberOfCells)
+            switch (sheet.GetRow(0).PhysicalNumberOfCells)
             {
                 case 2:
                     for (i = 0; i <= sheet.LastRowNum; i++)
                     {
-                        row = sheet.GetRow(i);
-                        if (row.GetCell(0).CellType != CellType.String || row.GetCell(1).CellType != CellType.String || String.IsNullOrWhiteSpace(row.GetCell(0).StringCellValue) == true || String.IsNullOrWhiteSpace(row.GetCell(1).StringCellValue) == true)
+                        if (sheet.GetRow(i).GetCell(0).CellType != CellType.String ||
+                            sheet.GetRow(i).GetCell(1).CellType != CellType.String ||
+                            String.IsNullOrWhiteSpace(sheet.GetRow(i).GetCell(0).StringCellValue) ||
+                            String.IsNullOrWhiteSpace(sheet.GetRow(i).GetCell(1).StringCellValue))
+                        {
+                            log.Warn($"Error in row {i}");
                             response = false;
+                        }
                     }
                     break;
                 case 4:
@@ -98,11 +85,15 @@ namespace AtecoSynchronizer
                 case 8:
                     for (i = 0; i <= sheet.LastRowNum; i++)
                     {
-                        row = sheet.GetRow(i);
-                        if (row.GetCell(0).StringCellValue == "XX")
-                            break;
-                        if (row.GetCell(0).CellType != CellType.String || row.GetCell(2).CellType != CellType.String || String.IsNullOrWhiteSpace(row.GetCell(0).StringCellValue) == true  || String.IsNullOrWhiteSpace(row.GetCell(2).StringCellValue) == true)
+                        if (sheet.GetRow(i).GetCell(0).StringCellValue != "XX" &&
+                            (sheet.GetRow(i).GetCell(0).CellType != CellType.String ||
+                            sheet.GetRow(i).GetCell(2).CellType != CellType.String || 
+                            String.IsNullOrWhiteSpace(sheet.GetRow(i).GetCell(0).StringCellValue) || 
+                            String.IsNullOrWhiteSpace(sheet.GetRow(i).GetCell(2).StringCellValue)))
+                        {
+                            log.Warn($"Error in row {i}");
                             response = false;
+                        }
                     }
                     break;
                 default:
@@ -113,45 +104,23 @@ namespace AtecoSynchronizer
         }
         public static List<Tuple<string, string>> Convert(string path)
         {
-            FileStream input = new FileStream(path, FileMode.Open, FileAccess.Read);
-            if (path.EndsWith(".xls"))
-            {
-                HSSFWorkbook workbook = new HSSFWorkbook(input);
-                ISheet sheet = workbook.GetSheetAt(0);
-                return ConversionInstructionSet(sheet);
-            }
-            else
-            {
-                XSSFWorkbook workbook = new XSSFWorkbook(input);
-                ISheet sheet = workbook.GetSheetAt(0);
-                return ConversionInstructionSet(sheet);
-            }
+            log.Info("Opening to convert " + path);
+            return path.EndsWith(".xls") ? 
+                ConversionInstructionSet(new HSSFWorkbook(new FileStream(path, FileMode.Open, FileAccess.Read)).GetSheetAt(0)) : 
+                ConversionInstructionSet(new XSSFWorkbook(new FileStream(path, FileMode.Open, FileAccess.Read)).GetSheetAt(0));
         }
         public static List<Tuple<string, string>> ConversionInstructionSet(ISheet sheet)
         {
-            var result = new List<Tuple<string, string>>(sheet.LastRowNum);
-            int i;
-            IRow row = sheet.GetRow(0);
-            if (row.PhysicalNumberOfCells == 2)
+            log.Info("Converting file");
+            List<Tuple< string, string>> result = new List<Tuple<string, string>>();
+            foreach (IRow row in sheet)
             {
-                for(i=0; i <= sheet.LastRowNum; i++)
-                {
-                    row = sheet.GetRow(i);
-                    var item = new Tuple<string, string>(row.GetCell(0).StringCellValue, row.GetCell(1).StringCellValue);
-                    result.Add(item);
-                }
+                if (row.PhysicalNumberOfCells == 2)
+                    result.Add(new Tuple<string, string> (row.GetCell(0).StringCellValue, row.GetCell(1).StringCellValue));
+                else if (row.GetCell(0).StringCellValue != "XX")
+                    result.Add(new Tuple<string, string>(row.GetCell(0).StringCellValue, row.GetCell(2).StringCellValue));
             }
-            else
-            {
-                for(i=0; i <= sheet.LastRowNum; i++)
-                {
-                    row = sheet.GetRow(i);
-                    if (row.GetCell(0).StringCellValue == "XX")
-                        break;
-                    var item = new Tuple<string, string>(row.GetCell(0).StringCellValue, row.GetCell(2).StringCellValue);
-                    result.Add(item);
-                }
-            }
+            log.Info("Converted");
             return result;
         }
     }
